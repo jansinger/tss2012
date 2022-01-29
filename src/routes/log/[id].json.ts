a0865972f8ddf0f02f6850c0fe882b6667bd4803
Jan Singer <jan.singer@singer-tc.de>
2022-01-29 13:12:49 +0100

    feat: add prev / next

--- src/routes/log/[id].json.ts
+++ src/routes/log/[id].json.ts
@@ -1,14 +1,24 @@
 import logbookEntries from '$lib/data/logbook.json';
 
+const sortedEntries = logbookEntries.sort(
+	(a, b) => new Date(a.datetime).getTime() - new Date(b.datetime).getTime()
+);
+
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const get = async ({ params }) => {
 	const { id } = params;
-	const entry = logbookEntries.find((row) => row._id === id);
-	if (!entry) {
+	const index = sortedEntries.findIndex((row) => row._id === id);
+	if (index === -1) {
 		return { status: 404, body: 'Not Found' };
 	}
+	const prev = index > 0 ? sortedEntries[index - 1]._id : undefined;
+	const next = index < sortedEntries.length - 1 ? sortedEntries[index + 1]._id : undefined;
 	return {
 		status: 200,
-		body: entry
+		body: {
+			_prev: prev,
+			_next: next,
+			...sortedEntries[index]
+		}
 	};
 };
