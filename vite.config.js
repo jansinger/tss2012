c9f64a155054398e9706724a274c409c4d8ad99b
Jan Singer <jan.singer@singer-tc.de>
2022-07-10 10:31:41 +0200

    feat: use vite instead of svelte-kit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	build: {
+		chunkSizeWarningLimit: 1000
+	}
+};
+
+export default config;
