e1bd70c390e58e834e0bce6a940f3158f17da615
Jan Singer <jan.singer@singer-tc.de>
2024-01-18 08:38:11 +0100

    feat: add testing

--- src/routes/log/[id]/+page.server.ts
+++ src/routes/log/[id]/+page.server.ts
@@ -1,4 +1,5 @@
 import { sortedEntries } from '$lib/sortedEntries';
+import { error, json } from '@sveltejs/kit';
 export const prerender = true;
 
 /** @type {import('./$types').PageServerLoad} */
@@ -7,7 +8,7 @@ export const load = async ({ params }) => {
 	
 	const index = sortedEntries.findIndex((row) => row._id === id);
 	if (index === -1) {
-		return { status: 404, errors: 'Not Found' };
+		return error(404, 'Not Found');
 	}
 	const prev = index > 0 ? sortedEntries[index - 1]._id : undefined;
 	const next = index < sortedEntries.length - 1 ? sortedEntries[index + 1]._id : undefined;
---
+++ src/routes/log/[id]/page.server.spec.ts
@@ -0,0 +1,34 @@
+import { sortedEntries } from '$lib/sortedEntries';
+import { load } from './+page.server';
+import { expect, test, describe } from 'vitest'
+
+describe('load function', () => {
+	test('should return entry object with previous and next values if id exists in sortedEntries', async () => {
+		const id = '82014b17-e618-4b5d-a244-140298f69916';
+		const result = await load({ params: { id } });
+
+		const index = sortedEntries.findIndex((row) => row._id === id);
+		const prev = index > 0 ? sortedEntries[index - 1]._id : undefined;
+		const next = index < sortedEntries.length - 1 ? sortedEntries[index + 1]._id : undefined;
+
+		expect(result).toEqual({
+			entry: {
+				_prev: prev,
+				_next: next,
+				...sortedEntries[index]
+			}
+		});
+	});
+
+	test('should return status 404 and errors "Not Found" if id does not exist in sortedEntries', async () => {
+		const id = 'nonExistentId';
+		try {
+			await load({ params: { id } });
+		} catch (error) {
+		expect(error).toMatchObject({
+			status: 404,
+			body: { message: 'Not Found' }
+		});
+	}
+	});
+});
