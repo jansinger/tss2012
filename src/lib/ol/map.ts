1e67385a72bdec1b8974fc14418664a8eb2dbcbc
Jan Singer <jan.singer@singer-tc.de>
2024-05-17 15:39:58 +0200

    chore: documented function

--- src/lib/ol/map.ts
+++ src/lib/ol/map.ts
@@ -8,6 +8,14 @@ import { DEFAULTS } from './constants';
 
 export const startLonLat = fromLonLat([DEFAULTS.lon, DEFAULTS.lat]);
 
+/**
+ * Creates a new map instance.
+ *
+ * @param target - The target element or ID where the map will be rendered.
+ * @param zoom - The initial zoom level of the map. Defaults to `DEFAULTS.zoom`.
+ * @param center - The initial center coordinates of the map. Defaults to `startLonLat`.
+ * @returns A new instance of the Map class.
+ */
 export const createMap = (
 	target: string | HTMLElement,
 	zoom: number = DEFAULTS.zoom,
